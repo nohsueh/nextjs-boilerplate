83f059cf3fab7456fbdcd7e6ad052458e9352465
nohsueh <nohsueh@qq.com>
2025-03-24 15:41:43 +0800

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  rewrites: async () => {
+    return [
+      {
+        source: "/blog/:path*",
+        destination: "https://blog-builder-demo.vercel.app/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
